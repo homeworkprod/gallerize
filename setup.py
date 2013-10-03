118094505d02c57223959b24395d54b11e398fda
Jochen Kupperschmidt <homework@nwsnet.de>
2013-10-03 14:58:51 +0200

    Created setup file and added tox configuration file.

---
+++ setup.py
@@ -0,0 +1,19 @@
+#!/usr/env/bin python
+
+from setuptools import setup
+
+
+setup(
+    name='gallerize',
+    version='0.3',
+    description='Create a static HTML/CSS image gallery from a bunch of images.',
+    author='Jochen Kupperschmidt',
+    author_email='homework@nwsnet.de',
+    url='http://homework.nwsnet.de/releases/cc0e/#gallerize',
+    install_requires=[
+        'jinja2 >= 2.6',
+    ],
+    test_requires=[
+        'pytest >= 2.4.1',
+    ],
+)
