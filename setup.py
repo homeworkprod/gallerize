52dc4fe9618aef6434f8f7d5fdaf592bb81c1fbe
Jochen Kupperschmidt <homework@nwsnet.de>
2015-08-09 19:53:04 +0200

    Include README as long package description. Specified license.

--- setup.py
+++ setup.py
@@ -1,12 +1,19 @@
-#!/usr/bin/env python
+# -*- coding: utf-8 -*-
 
 from setuptools import setup
+
+
+def read_readme():
+    with open('README.rst') as f:
+        return f.read()
 
 
 setup(
     name='gallerize',
     version='0.3.1',
     description='Create a static HTML/CSS image gallery from a bunch of images.',
+    long_description=read_readme(),
+    license='MIT',
     author='Jochen Kupperschmidt',
     author_email='homework@nwsnet.de',
     url='http://homework.nwsnet.de/releases/cc0e/#gallerize',
